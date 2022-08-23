a15e8c009797cc93abc9486aad98dac9efa9ce1c
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-08-23 13:50:33 -0600

    Código fuente base

---
+++ 20220823/Base/MyMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+using System;
+
+namespace WA11
+{
+    public class MyMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        public MyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {            
+            context.Response.Headers.Add("X-MyMiddleware", 
+                new StringValues($"Welcome at {DateTime.Now.ToString()}!"));
+            await _next.Invoke(context);
+        }
+    }
+}
