a2c8e1fa2b9c05349ddbf745221d8337b694235c
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-09-20 20:06:48 -0600

    Actualización

--- 20220920/WA50/WA50/Controllers/HomeController.cs
+++ 20220920/WA50/WA50/Controllers/HomeController.cs
@@ -12,9 +12,47 @@ namespace WA50.Controllers
         {
             _logger = logger;
         }
-                
-        public IActionResult Index()
+
+        public IActionResult Index(HomeIndexModel m)
         {
+            if (m.Filter != "")
+            {
+                using (var db = new Northwind.Store.Data.NWContext())
+                {
+                    m.Items = db.Products.Where(p => p.ProductName.Contains(m.Filter)).ToList();
+                }
+            }
+
+            return View(m);
+        }
+
+        //[HttpPost]
+        //[HttpGet]
+        //public IActionResult Index([FromRoute] string filter="")
+        //public IActionResult Index(string filter = "")
+        public IActionResult Index1(HomeIndexModel m)
+        {
+            //List<Northwind.Store.Model.Product> result = new();
+
+            //if (filter != "")
+            if (m.Filter != "")
+            {
+                using (var db = new Northwind.Store.Data.NWContext())
+                {
+                    //result = db.Items.Where(p => p.ProductName.Contains(filter)).ToList();
+
+                    m.Items = db.Products.Where(p => p.ProductName.Contains(m.Filter)).ToList();
+                }
+            }
+
+            return View(m);
+        }
+
+        public IActionResult Index0(string filter)
+        {
+            ViewData[nameof(filter)] = filter;
+            ViewBag.filter2 = filter;
+
             return View();
         }
 
