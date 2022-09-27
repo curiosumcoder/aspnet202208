b61b474df94c8306da44f77f21ce2aca7d1933bf
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-09-27 14:24:21 -0600

    libman

---
+++ 20220927/WA50/WA50/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WA50.Models;
+
+namespace WA50.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(HomeIndexModel m)
+        {
+            if (m.Filter != "")
+            {
+                using (var db = new Northwind.Store.Data.NWContext())
+                {
+                    m.Items = db.Products.Where(p => p.ProductName.Contains(m.Filter)).ToList();
+                }
+            }
+
+            return View(m);
+        }
+
+        //[HttpPost]
+        //[HttpGet]
+        //public IActionResult Index([FromRoute] string filter="")
+        //public IActionResult Index(string filter = "")
+        public IActionResult Index1(HomeIndexModel m)
+        {
+            //List<Northwind.Store.Model.Product> result = new();
+
+            //if (filter != "")
+            if (m.Filter != "")
+            {
+                using (var db = new Northwind.Store.Data.NWContext())
+                {
+                    //result = db.Items.Where(p => p.ProductName.Contains(filter)).ToList();
+
+                    m.Items = db.Products.Where(p => p.ProductName.Contains(m.Filter)).ToList();
+                }
+            }
+
+            return View(m);
+        }
+
+        public IActionResult Index0(string filter)
+        {
+            ViewData[nameof(filter)] = filter;
+            ViewBag.filter2 = filter;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 20220927/WA50/WA50/Models/HomeIndexModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WA50.Models
+{
+    public class HomeIndexModel
+    {
+        public string Filter { get; set; } = "";
+        public List<Northwind.Store.Model.Product> Items { get; set; }
+    }
+}
