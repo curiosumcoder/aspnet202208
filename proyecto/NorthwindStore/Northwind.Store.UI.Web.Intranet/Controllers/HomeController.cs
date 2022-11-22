98caefa98e276e0ea1e36acb2d0b547c20a18d78
Gilberto Bermúdez Garro <gbermude@outlook.com>
2022-11-22 16:49:17 -0600

    Update

--- proyecto/NorthwindStore/Northwind.Store.UI.Web.Intranet/Controllers/HomeController.cs
+++ proyecto/NorthwindStore/Northwind.Store.UI.Web.Intranet/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Store.UI.Web.Intranet.Models;
 using System.Diagnostics;
@@ -17,6 +18,8 @@ namespace Northwind.Store.UI.Web.Intranet.Controllers
 
         public IActionResult Index()
         {
+            throw new ApplicationException("Tenemos un problema");
+
             return View();
         }
 
@@ -28,7 +31,52 @@ namespace Northwind.Store.UI.Web.Intranet.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            //return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+            var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            var exceptionMessage = "";
+
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (exceptionHandlerPathFeature?.Error is ApplicationException)
+            {
+                var ex = exceptionHandlerPathFeature?.Error;
+
+                exceptionMessage = $"Error en la aplicación: {ex.Message}";
+                _logger.LogError(exceptionMessage);
+            }
+            else if (exceptionHandlerPathFeature?.Error is Microsoft.Data.SqlClient.SqlException)
+            {
+                var ex = exceptionHandlerPathFeature?.Error;
+                exceptionMessage = $"Error en la SQL Server: {ex.Message}";
+                _logger.LogError(exceptionMessage);
+            }
+
+            if (exceptionHandlerPathFeature?.Path == "/")
+            {
+                exceptionMessage += " desde la raíz.";
+            }
+
+            return View("Error", new ErrorViewModel { RequestId = requestId });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult ErrorWithCode(string code)
+        {
+            var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            var statusCodeReExecuteFeature = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            if (statusCodeReExecuteFeature != null)
+            {
+                _ =
+                    statusCodeReExecuteFeature.OriginalPathBase
+                    + statusCodeReExecuteFeature.OriginalPath
+                    + statusCodeReExecuteFeature.OriginalQueryString;
+            }
+
+            ViewBag.statusCode = code;
+
+            return View("ErrorStatus", new ErrorViewModel { RequestId = requestId });
         }
     }
 }
